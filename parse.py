0998d1d2ca7aec5722f91a0b0ecc189529931a1c
HeeRyun, Jin <jins4218@gmail.com>
2018-12-23 00:38:38 +0900

    JBoss 로그인 구현

---
+++ parse.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+login_data = []
+f = open("enc_login.txt", 'r')
+lines = f.readlines()
+for line in lines:
+    line = line.replace("\n", "")
+    login_data.append(line)
+f.close()
+
+res = requests.post("http://smartcampus.deu.ac.kr/auth/user/loginAuth", data={'entryA': login_data[0], 'entryB': login_data[1], 'strongbox': 'deusmartcampus', 'ip': 'dummy'})
+
+res = BeautifulSoup(res.text, "lxml")
+name = res.findAll("suser_nm")
+id = res.findAll("suser_id")
+dept = res.findAll("dept_nm")
+
+print("이름 : " + name[0].text + "\n학번 : " + id[0].text + "\n학과 : " + dept[0].text + "\n")
