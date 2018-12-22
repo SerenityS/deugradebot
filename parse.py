3d178bc3b09aec83d4a15e7c507a361163196d74
HeeRyun, Jin <jins4218@gmail.com>
2018-12-23 01:03:31 +0900

    Josso 로그인 구현
    Signed-off-by: HeeRyun, Jin <jins4218@gmail.com>

--- parse.py
+++ parse.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 login_data = []
-f = open("enc_login.txt", 'r')
+f = open("login.txt", 'r')
 lines = f.readlines()
 for line in lines:
     line = line.replace("\n", "")
@@ -10,12 +10,13 @@ for line in lines:
 f.close()
 
 with requests.Session() as s:
-    res = s.post("http://smartcampus.deu.ac.kr/auth/user/loginAuth", data={'entryA': login_data[0], 'entryB': login_data[1], 'strongbox': 'deusmartcampus'})
-    print(res.cookies)
+    jboss = s.post("http://smartcampus.deu.ac.kr/auth/user/loginAuth", data={'entryA': login_data[2], 'entryB': login_data[3], 'strongbox': 'deusmartcampus'})
 
-    res = BeautifulSoup(res.text, "lxml")
-    name = res.findAll("suser_nm")
-    id = res.findAll("suser_id")
-    dept = res.findAll("dept_nm")
+    jboss = BeautifulSoup(jboss.text, "lxml")
+    name = jboss.findAll("suser_nm")
+    id = jboss.findAll("suser_id")
+    dept = jboss.findAll("dept_nm")
 
     print("이름 : " + name[0].text + "\n학번 : " + id[0].text + "\n학과 : " + dept[0].text + "\n")
+
+    josso = s.post('http://smartcampus.deu.ac.kr/josso/signon/usernamePasswordLogin.do', data={'josso_cmd': 'login', 'josso_username': login_data[0], 'josso_password': login_data[1]})
