704553982e1a9ff24b37834e371e36dbdb287d93
HeeRyun, Jin <jins4218@gmail.com>
2018-12-26 03:12:24 +0900

    필요 없는 print 삭제
    Signed-off-by: HeeRyun, Jin <jins4218@gmail.com>

--- parse.py
+++ parse.py
@@ -4,17 +4,8 @@ from login_data import login_data
 
 def get_score_data():
     with requests.Session() as s:
-        jboss = s.post("http://smartcampus.deu.ac.kr/auth/user/loginAuth", data={'entryA': login_data[2], 'entryB': login_data[3], 'strongbox': 'deusmartcampus'})
-
-        jboss = BeautifulSoup(jboss.text, "lxml")
-        name = jboss.findAll("suser_nm")
-        id = jboss.findAll("suser_id")
-        dept = jboss.findAll("dept_nm")
-
-        print("이름 : " + name[0].text + "\n학번 : " + id[0].text + "\n학과 : " + dept[0].text + "\n")
-
+        s.post("http://smartcampus.deu.ac.kr/auth/user/loginAuth", data={'entryA': login_data[2], 'entryB': login_data[3], 'strongbox': 'deusmartcampus'})
         josso = s.post('http://smartcampus.deu.ac.kr/josso/signon/usernamePasswordLogin.do', data={'josso_cmd': 'login', 'josso_username': login_data[0], 'josso_password': login_data[3]})
-
         res = s.get('http://smartcampus.deu.ac.kr/webservice/302/D/01/01', cookies=josso.cookies)
 
         score = BeautifulSoup(res.text, "lxml")
